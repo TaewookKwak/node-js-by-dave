c2d2bb8633ea6369ca8e1da4028e57122209181a
Taewook <rhkrxodnr@gmail.com>
2022-04-08 16:02:15 +0900

    [ch5] init

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+
+const PORT = process.env.PORT || 3500
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method)
+
+  let path
+  if (req.url === '/' || req.url === 'index.html') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    path = path.join(__dirname, 'views', 'index.html')
+    fs.readFile(path, 'utf-8', (err, data) => {
+      res.end(data)
+    })
+  }
+})
+
+server.listen(PORT, () => console.log('Server running on port : ' + PORT))
